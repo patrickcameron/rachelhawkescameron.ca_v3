871a8a1ce6747967064a470879d9047ac17401d5
Patrick Cameron <patcameron@gmail.com>
2019-07-14 13:34:24 -0400

    Add Commissions page to header menu

--- src/components/header.js
+++ src/components/header.js
@@ -29,7 +29,7 @@ class Header extends React.Component {
                     <ul id="js--header-nav">
                         { !isHome && <li className="home-link"><Link to="/">Home</Link></li> }
                         <li><Link to="/about">About</Link></li>
-                        {/* <li><a href="/commissions">Commissions</a></li> */}
+                        <li><a href="/commissions">Commissions</a></li>
                         <li><a href="mailto:rachelcameron@gmail.com">Contact</a></li>
                         <li><a href="https://www.etsy.com/ca/shop/rachelhawkescameron" target="_blank" rel="noopener noreferrer">Etsy</a></li>
                         <li><a href="https://www.instagram.com/rachelhawkescameronart/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
